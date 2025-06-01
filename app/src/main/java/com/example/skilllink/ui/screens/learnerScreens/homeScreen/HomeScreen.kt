989750ba6eeb6919d2cec3f50e1d43f4047765b2
JatinJain123 <jatinjain0529@gmail.com>
2025-06-02 03:08:46 +0530

    created Tending Scroll in Home screen

--- app/src/main/java/com/example/skilllink/ui/screens/learnerScreens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/skilllink/ui/screens/learnerScreens/homeScreen/HomeScreen.kt
@@ -3,23 +3,15 @@ package com.example.skilllink.ui.screens.learnerScreens.homeScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,19 +20,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.skilllink.domain.model.local.LocalAppDependencies
-import com.example.skilllink.ui.reusableComponents.cards.CircularReelsCard
 import com.example.skilllink.ui.theme.CustomFields
 import com.example.skilllink.ui.theme.LocalCustomColors
 import com.example.skilllink.ui.theme.SkillLinkTheme
 import com.example.skilllink.ui.theme.VeryLightGray
-import com.example.skilllink.utils.testReelData
 
 
 @Composable
@@ -108,10 +95,10 @@ fun ScreenView(
                 navController = navController
             )
 
-//            TrendingScroll(
-//                customFields = customFields,
-//                navController = navController
-//            )
+            TrendingScroll(
+                customFields = customFields,
+                navController = navController
+            )
         }
     }
 }
